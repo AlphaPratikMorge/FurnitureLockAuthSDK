67dd2ea5a8bca8717984c36a854ef43cacfc89c3
HiteshAlpha <hitesh.fuse@alpha-ict.in>
2024-02-28 15:01:17 +0530

    Initial Commit

---
+++ AdvantisVaveAuthSDK/AdvantisVaveAuthSDK.h
@@ -0,0 +1,18 @@
+//
+//  AdvantisVaveAuthSDK.h
+//  AdvantisVaveAuthSDK
+//
+//  Created by Alpha-MacPro on 28/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdvantisVaveAuthSDK.
+FOUNDATION_EXPORT double AdvantisVaveAuthSDKVersionNumber;
+
+//! Project version string for AdvantisVaveAuthSDK.
+FOUNDATION_EXPORT const unsigned char AdvantisVaveAuthSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvantisVaveAuthSDK/PublicHeader.h>
+
+
